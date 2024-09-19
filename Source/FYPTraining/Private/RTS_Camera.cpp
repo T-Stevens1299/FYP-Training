be9f31cfe581808301dbcfb95ed60779696f460d
Thomas Stevens <s012473l@student.staffs.ac.uk>
2024-09-19 11:14:49 +0100

    Camera Bug Fix
    Camera now moves the correct way depending on camera rotation

--- Source/FYPTraining/Private/RTS_Camera.cpp
+++ Source/FYPTraining/Private/RTS_Camera.cpp
@@ -76,7 +76,7 @@ void ARTS_Camera::MoveCamera(const FInputActionValue& Value)
 
 	if (Controller != nullptr)
 	{
-		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator Rotation = CameraArm->GetRelativeRotation();
 		const FRotator YawRotation(0, Rotation.Yaw, 0);
 
 		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
